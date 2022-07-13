afea98a98c53b9fef062dcbc1b51ee7fe5700896
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-07-13 13:32:22 +0430

    It works great.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final int size = 3;
+    private static final String inputError = "Invalid input\ntry again";
 
     public static void main(String[] args) {
 
         System.out.println("Enter a number of users: ");
-        int numberOfUsers = getIntFromInput(Integer.MAX_VALUE);
+        int numberOfUsers = getIntFromInput();
 
 
         System.out.println("Enter a number of dimensions: ");
-        int dimensions = getIntFromInput(Integer.MAX_VALUE);
+        int dimensions = getIntFromInput();
+        scanner.nextLine();
         String[][] board = new String[dimensions][dimensions];
         for (int  i = 0; i < dimensions; i++)
             Arrays.fill(board[i], " ");
@@ -33,8 +38,9 @@ public class Main {
                         "Please choose i and j as \"i j\" format which i and j are between 1 and " + dimensions +
                         " (i is row number and j is column number): ");
 
-                row = getIntFromInput(dimensions) - 1;
-                column = getIntFromInput(dimensions) - 1;
+                int[] coordinates = getCoordinatesFromInput(dimensions);
+                row = coordinates[0] - 1;
+                column = coordinates[1] - 1;
 
                 if (board[row][column].equals(" ")) {
                     board[row][column] = String.valueOf(i);
@@ -56,7 +62,7 @@ public class Main {
             System.out.println("Winner is " + winner);
     }
 
-    public static int getIntFromInput(int checkValue) {
+    public static int getIntFromInput() {
         boolean isInputValid = false;
         int number = 0;
 
@@ -65,16 +71,41 @@ public class Main {
 
             try {
                 number =  Integer.parseInt(input);
-                if (1 <= number && number <= checkValue)
+                if (1 < number)
                     isInputValid = true;
                 else
                     throw new NumberFormatException();
             } catch (Exception e) {
-                System.out.println("Invalid input\ntry again");
+                System.out.println(inputError);
             }
         }
 
         return number;
+    }
+
+    public static int[] getCoordinatesFromInput(int checkValue) {
+        boolean isInputValid = false;
+        int[] coordinates = new int[2];
+
+        while (!isInputValid) {
+            String input = scanner.nextLine();
+
+            Matcher inputMatcher = Pattern.compile("^(?<i>\\d+) (?<j>\\d+)$").matcher(input);
+            if (inputMatcher.find()) {
+                coordinates[0] = Integer.parseInt(inputMatcher.group("i"));
+                coordinates[1] = Integer.parseInt(inputMatcher.group("j"));
+
+                if (1 <= coordinates[0] && coordinates[0] <= checkValue &&
+                        1 <= coordinates[1] && coordinates[1] <= checkValue)
+                    isInputValid = true;
+                else
+                    System.out.println(inputError);
+            } else {
+                System.out.println(inputError);
+            }
+        }
+
+        return coordinates;
     }
 
     public static void drawTable(int dimensions, String[][] board) {
@@ -160,7 +191,7 @@ public class Main {
         int coefficient = isMainDiameter ? 1 : -1;
 
         try {
-            if (board[row][column].equals(board[row - coefficient * 1][column + 1]) &&
+            if (board[row][column].equals(board[row - coefficient][column + 1]) &&
                     board[row][column].equals(board[row - coefficient * 2][column + 2])) {
                 return true;
             }
@@ -168,15 +199,15 @@ public class Main {
         }
 
         try {
-            if (board[row][column].equals(board[row + coefficient * 1][column - 1]) &&
-                    board[row][column].equals(board[row - coefficient * 1][column + 1])) {
+            if (board[row][column].equals(board[row + coefficient][column - 1]) &&
+                    board[row][column].equals(board[row - coefficient][column + 1])) {
                 return true;
             }
         } catch (Exception ignored) {
         }
 
         try {
-            if (board[row][column].equals(board[row + coefficient * 1][column - 1]) &&
+            if (board[row][column].equals(board[row + coefficient][column - 1]) &&
                     board[row][column].equals(board[row + coefficient * 2][column - 2])) {
                 return true;
             }
